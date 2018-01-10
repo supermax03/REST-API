4700a1d9cf87a265ade808acfb28ec502cae9859
supermax03 <maximiliano.bordon@gmail.com>
2018-01-10 17:59:08 -0300

    Add database files

---
+++ database/farm.sql
@@ -0,0 +1,15 @@
+﻿-- Table: farm
+
+-- DROP TABLE farm;
+
+CREATE TABLE farm
+(
+  name character varying(120) NOT NULL,
+  address character varying(120),
+  CONSTRAINT farm_pk PRIMARY KEY (name)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE farm
+  OWNER TO postgres;
